d0256f7e203558910f8e793fb505b992fcb2dc39
Dennis-kom <55036289+Dennis-kom@users.noreply.github.com>
2021-05-28 23:00:36 +0300

    HW 14 Q1 UPDATE

---
+++ Tester.java
@@ -0,0 +1,16 @@
+package HW14;
+
+public class Tester {
+
+    public static void main(String[]args){
+         boolean[][]mat1 = new boolean[5][5];
+
+         for(int i=0;i<5;i++){
+             for(int j=0;j<5;j++){
+                 if((i==0 && (j==1 || j==4)) || (i==1 && (j==0 || j==3 || j==4)) || (i==2 && (j==2 || j==3)) || (i==3 && j==0) || (i==4 && (j==0 || j==1 || j==2))){ mat1[i][j] = true;}
+                 else{mat1[i][j] = false;}
+             }
+         }
+        System.out.println(Ex14.size(mat1,0,0));
+    }
+}
